32f068cb2747101c2f932f639e74e1e62a191d3d
Daniel Lindström <daniel.lindstrom@ericsson.com>
2014-10-06 16:43:36 +0200

    owr_transport_agent: Removed use of removed property.

--- transport/owr_transport_agent.c
+++ transport/owr_transport_agent.c
@@ -287,7 +287,7 @@ static void owr_transport_agent_get_property(GObject *object, guint property_id,
 OwrTransportAgent * owr_transport_agent_new(gboolean ice_controlling_mode)
 {
     return g_object_new(OWR_TYPE_TRANSPORT_AGENT, "ice-controlling_mode", ice_controlling_mode,
-        "dtls-client-mode", NULL);
+        NULL);
 }
 
 /**
