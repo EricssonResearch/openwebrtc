1feed3178ae36dbc3683c8b0e6e645f32650e3cf
Sebastian Dröge <sebastian@centricular.com>
2014-10-29 13:49:01 +0100

    transport: Don't put a capsfilter in front of the audio encoder
    We already have the same capsfilter in the source.

--- transport/owr_transport_agent.c
+++ transport/owr_transport_agent.c
@@ -1271,7 +1271,7 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
     guint stream_id;
     OwrPayload *payload = NULL;
     GstElement *send_input_bin = NULL;
-    GstElement *capsfilter = NULL, *encoder = NULL, *parser = NULL, *payloader = NULL,
+    GstElement *encoder = NULL, *parser = NULL, *payloader = NULL,
         *rtp_capsfilter = NULL, *rtpbin = NULL;
     GstCaps *caps = NULL, *rtp_caps = NULL;
     gchar *name = NULL;
@@ -1374,20 +1374,11 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
         gst_object_unref(sink_pad);
         g_free(name);
     } else { /* Audio */
-        name = g_strdup_printf("send_capsfilter_%u", stream_id);
-        capsfilter = gst_element_factory_make("capsfilter", name);
-        g_free(name);
-
-        caps = _owr_payload_create_raw_caps(payload);
-        g_object_set(capsfilter, "caps", caps, NULL);
-        gst_caps_unref(caps);
-
         encoder = _owr_payload_create_encoder(payload);
         parser = _owr_payload_create_parser(payload);
         payloader = _owr_payload_create_payload_packetizer(payload);
 
-        gst_bin_add_many(GST_BIN(send_input_bin), capsfilter, encoder, payloader, NULL);
-        link_ok = gst_element_link_many(capsfilter, encoder, NULL);
+        gst_bin_add_many(GST_BIN(send_input_bin), encoder, payloader, NULL);
         if (parser) {
             gst_bin_add(GST_BIN(send_input_bin), parser);
             link_ok &= gst_element_link_many(encoder, parser, payloader, NULL);
@@ -1402,11 +1393,10 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
         if (parser)
             sync_ok &= gst_element_sync_state_with_parent(parser);
         sync_ok &= gst_element_sync_state_with_parent(encoder);
-        sync_ok &= gst_element_sync_state_with_parent(capsfilter);
         g_warn_if_fail(sync_ok);
 
         name = g_strdup_printf("audio_raw_sink_%u", stream_id);
-        sink_pad = gst_element_get_static_pad(capsfilter, "sink");
+        sink_pad = gst_element_get_static_pad(encoder, "sink");
         add_pads_to_bin_and_transport_bin(sink_pad, send_input_bin,
             transport_agent->priv->transport_bin, name);
         gst_object_unref(sink_pad);
