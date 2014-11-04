8a688e2595cd2c7187f6b55af1f66233e3cced70
Arun Raghavan <arun@centricular.com>
2014-11-04 15:01:13 +0530

    tests: Add manual device selection to test-self-view
    Off by default since it's probably not needed other than for device
    listing testing.

--- tests/test_self_view.c
+++ tests/test_self_view.c
@@ -37,7 +37,12 @@
 static OwrMediaSource *audio_source = NULL, *video_source = NULL;
 static OwrMediaRenderer *audio_renderer = NULL, *video_renderer = NULL;
 
-static gboolean dump_pipeline(gpointer user_data)
+#define MANUAL_SOURCE_SELECTION 0
+
+/* For reading source selection from console in manual mode*/
+#include <stdio.h>
+
+gboolean dump_pipeline(gpointer user_data)
 {
     g_print("Dumping pipelines\n");
 
@@ -53,25 +58,66 @@ static gboolean dump_pipeline(gpointer user_data)
     return FALSE;
 }
 
-static void got_sources(GList *sources, gpointer user_data)
+static OwrMediaSource * pick_a_source(GList *sources, OwrMediaType type, gboolean manual)
 {
+    GList *item = sources;
     OwrMediaSource *source = NULL;
+    OwrSourceType source_type;
+    guint i = 1, sel = 0;
+    gboolean have_one = FALSE;
+
+    while (item) {
+        gchar *name;
+        OwrMediaType media_type;
+
+        source = OWR_MEDIA_SOURCE(item->data);
+        g_object_get(source, "name", &name, "media-type", &media_type, "type", &source_type, NULL);
+
+        if (source_type == OWR_SOURCE_TYPE_CAPTURE && media_type == type) {
+            if (manual) {
+                g_print("%u: %s (%s)\n", i, name, media_type == OWR_MEDIA_TYPE_VIDEO ? "video" : "audio");
+                have_one = TRUE;
+            } else {
+                g_free(name);
+                return source;
+            }
+        }
+
+        item = item->next;
+        i++;
+
+        g_free(name);
+    }
+
+    if (!have_one)
+        return NULL;
+
+    scanf("%u", &sel);
+
+    if (sel < 1 || sel >= i)
+        return NULL;
+
+    return OWR_MEDIA_SOURCE((g_list_nth(sources, sel - 1))->data);
+}
+
+void got_sources(GList *sources, gpointer user_data)
+{
+    OwrMediaSource *source;
     static gboolean have_video = FALSE, have_audio = FALSE;
+
     g_assert(sources);
 
     g_print("Got sources!\n");
 
-    while(sources && (source = sources->data)) {
-        OwrMediaType media_type;
-        OwrSourceType source_type;
+    if (have_video && have_audio)
+        return;
 
-        g_assert(OWR_IS_MEDIA_SOURCE(source));
+    if (!have_video) {
+        OwrVideoRenderer *renderer;
 
-        g_object_get(source, "type", &source_type, "media-type", &media_type, NULL);
+        source = pick_a_source(sources, OWR_MEDIA_TYPE_VIDEO, MANUAL_SOURCE_SELECTION);
 
-        if (!have_video && media_type == OWR_MEDIA_TYPE_VIDEO && source_type == OWR_SOURCE_TYPE_CAPTURE) {
-            OwrVideoRenderer *renderer;
-
+        if (source) {
             have_video = TRUE;
 
             renderer = owr_video_renderer_new(NULL);
@@ -79,11 +125,18 @@ static void got_sources(GList *sources, gpointer user_data)
 
             g_object_set(renderer, "width", 1280, "height", 720, "max-framerate", 30.0, NULL);
             owr_media_renderer_set_source(OWR_MEDIA_RENDERER(renderer), source);
+
             video_renderer = OWR_MEDIA_RENDERER(renderer);
             video_source = source;
-        } else if (!have_audio && media_type == OWR_MEDIA_TYPE_AUDIO && source_type == OWR_SOURCE_TYPE_CAPTURE) {
-            OwrAudioRenderer *renderer;
+        }
+    }
 
+    if (!have_audio) {
+        OwrAudioRenderer *renderer;
+
+        source = pick_a_source(sources, OWR_MEDIA_TYPE_AUDIO, MANUAL_SOURCE_SELECTION);
+
+        if (source) {
             have_audio = TRUE;
 
             renderer = owr_audio_renderer_new();
@@ -93,11 +146,6 @@ static void got_sources(GList *sources, gpointer user_data)
             audio_renderer = OWR_MEDIA_RENDERER(renderer);
             audio_source = source;
         }
-
-        if (have_video && have_audio)
-            break;
-
-        sources = sources->next;
     }
 
     g_timeout_add(5000, dump_pipeline, NULL);
