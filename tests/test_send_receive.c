4b9f398e7c5914e4e00f52b68b667ee94e58c7d2
Arun Raghavan <arun@centricular.com>
2014-11-22 22:50:32 +0530

    tests: Enable retransmission for video in test-send-receive
    This is on by default here, but needs TEST_RTX to be defined in
    transport/owr_transport_agent.c to generate some actual data loss and
    trigger retransmission.

--- tests/test_send_receive.c
+++ tests/test_send_receive.c
@@ -121,6 +121,8 @@ static void got_sources(GList *sources, gpointer user_data)
 
             payload = owr_video_payload_new(OWR_CODEC_TYPE_VP8, 103, 90000, TRUE, FALSE);
             g_object_set(payload, "width", 1280, "height", 720, "framerate", 30.0, NULL);
+            g_object_set(payload, "rtx-payload-type", 123, NULL);
+
             owr_media_session_set_send_payload(send_session_video, payload);
             g_object_unref(payload);
 
@@ -213,6 +215,8 @@ int main() {
     g_signal_connect(recv_session_video, "on-incoming-source", G_CALLBACK(got_remote_source), NULL);
 
     receive_payload = owr_video_payload_new(OWR_CODEC_TYPE_VP8, 103, 90000, TRUE, FALSE);
+    g_object_set(receive_payload, "rtx-payload-type", 123, NULL);
+
     owr_media_session_add_receive_payload(recv_session_video, receive_payload);
     g_object_unref(receive_payload);
 
