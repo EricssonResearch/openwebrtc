64bb85877ed2c0e66dc7084e7f63e5a7e76c4b9a
Arun Raghavan <arun@centricular.com>
2014-11-27 12:03:14 +0530

    local: Namespace the _get_avf_*() functions
    Adding a _owr should avoid accidental namespace collisions in case of
    static builds.

--- local/owr_device_list_avf_private.h
+++ local/owr_device_list_avf_private.h
@@ -34,9 +34,9 @@
 
 G_BEGIN_DECLS
 
-GList *_get_avf_video_sources();
+GList *_owr_get_avf_video_sources();
 
-GList *_get_avf_audio_sources();
+GList *_owr_get_avf_audio_sources();
 
 G_END_DECLS
 
