13767ffe557c92b33f419beb636bd514b8de2ad8
Robert Swain <robert.swain@ericsson.com>
2014-12-08 09:06:42 +0100

    OwrPayload: Set good default vtenc_h264 properties

--- transport/owr_payload.c
+++ transport/owr_payload.c
@@ -346,6 +346,8 @@ GstElement * _owr_payload_create_encoder(OwrPayload *payload)
         } else if (!strcmp(factory_name, "vtenc_h264")) {
             g_object_bind_property_full(payload, "bitrate", encoder, "bitrate", G_BINDING_SYNC_CREATE,
                 binding_transform_to_kbps, NULL, NULL, NULL);
+            g_object_set(encoder, "allow-frame-reordering", FALSE, "realtime", TRUE,
+                "quality", 0.5, "max-keyframe-interval", G_MAXINT, NULL);
         } else {
             /* Assume bits/s instead of kbit/s */
             g_object_bind_property(payload, "bitrate", encoder, "bitrate", G_BINDING_SYNC_CREATE);
