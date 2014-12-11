655a41995e4de3a53f5d0c08a9c8b7e7f7457c5a
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2014-12-11 17:15:52 +0100

    build, jni: made libopenwebrtc_bridge_jni depend on libopenwebrtc_jni

--- gen_jni/bridge/com/ericsson/research/owr/OwrBridge.java
+++ gen_jni/bridge/com/ericsson/research/owr/OwrBridge.java
@@ -2,6 +2,7 @@ package com.ericsson.research.owr;
 
 public class OwrBridge {
     static {
+        System.loadLibrary("openwebrtc_jni");
         System.loadLibrary("openwebrtc_bridge_jni");
     }
     public static native void start();
