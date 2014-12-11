5968814fb0208cd646e16528ec96478d81cc5b67
Robert Swain <robert.swain@ericsson.com>
2014-12-11 10:05:46 +0100

    tests: Add a list-devices test app
    It simply writes the available devices to stdout in the following
    format:
    
    [video/capture] Back Camera

---
+++ tests/list_devices.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2014, Ericsson AB. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
+#define _GNU_SOURCE 1
+
+#include <string.h>
+
+#include "owr.h"
+#include "owr_local.h"
+#include "owr_media_source.h"
+
+static GMainLoop *loop;
+
+static void got_sources(GList *sources, gpointer user_data)
+{
+    OwrMediaSource *source = NULL;
+    g_assert(sources);
+
+    while(sources && (source = sources->data)) {
+        gchar *name = NULL;
+        OwrMediaType media_type;
+        OwrSourceType source_type;
+
+        g_assert(OWR_IS_MEDIA_SOURCE(source));
+
+        g_object_get(source, "name", &name, "type", &source_type, "media-type", &media_type, NULL);
+
+        g_print("[%s/%s] %s\n", media_type == OWR_MEDIA_TYPE_AUDIO ? "audio" : "video",
+            source_type == OWR_SOURCE_TYPE_CAPTURE ? "capture" : source_type == OWR_SOURCE_TYPE_TEST ? "test" : "unknown",
+            name);
+
+        sources = sources->next;
+    }
+
+    g_main_loop_quit(loop);
+}
+
+int main() {
+    GMainContext *ctx = g_main_context_default();
+
+    loop = g_main_loop_new(ctx, FALSE);
+
+    owr_init_with_main_context(ctx);
+
+    owr_get_capture_sources(OWR_MEDIA_TYPE_AUDIO|OWR_MEDIA_TYPE_VIDEO, got_sources, NULL);
+
+    g_main_loop_run(loop);
+
+    return 0;
+}
