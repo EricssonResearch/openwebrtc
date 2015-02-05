8cdc5fd8f95334d4c4b16318dbf3bcd63ab4f133
Robert Swain <robert.swain@ericsson.com>
2015-02-05 15:08:43 +0100

    OwrMediaSession: Reduce default jitter buffer latency to 50ms

--- transport/owr_media_session.c
+++ transport/owr_media_session.c
@@ -290,7 +290,7 @@ static void owr_media_session_class_init(OwrMediaSessionClass *klass)
     obj_properties[PROP_JITTER_BUFFER_LATENCY] = g_param_spec_uint("jitter-buffer-latency",
         "Session jitter buffer latency in ms",
         "The latency introduced by the jitter buffer for this session in ms",
-        0, G_MAXUINT, 200,
+        0, G_MAXUINT, 50,
         G_PARAM_STATIC_STRINGS | G_PARAM_READWRITE);
 
     g_object_class_install_properties(gobject_class, N_PROPERTIES, obj_properties);
@@ -313,7 +313,7 @@ static void owr_media_session_init(OwrMediaSession *media_session)
     priv->on_send_payload = NULL;
     priv->on_send_source = NULL;
     priv->remote_sources = NULL;
-    priv->jitter_buffer_latency = 200;
+    priv->jitter_buffer_latency = 50;
     g_mutex_init(&priv->remote_source_lock);
     g_rw_lock_init(&priv->rw_lock);
 }
