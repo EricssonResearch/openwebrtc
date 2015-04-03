084b9c86b21fa4df3d9c87c59eca9f2fe0c89bb0
Robert Swain <robert.swain@ericsson.com>
2015-04-03 19:39:43 +0200

    OwrURISourceAgent: OwrURISource: New API to play from a URI
    Note that only the available GStreamer plugins will be used and so
    format support is limited to that. On OS X, H.264 and AAC should work at
    least.

---
+++ local/owr_uri_source_private.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014-2015, Ericsson AB. All rights reserved.
+ * Copyright (c) 2014, Centricular Ltd
+ *     Author: Sebastian Dröge <sebastian@centricular.com>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
+/*/
+\*\ OwrURISource private
+/*/
+
+#ifndef __OWR_URI_SOURCE_PRIVATE_H__
+#define __OWR_URI_SOURCE_PRIVATE_H__
+
+#include "owr_media_source.h"
+#include "owr_types.h"
+
+#ifndef __GTK_DOC_IGNORE__
+
+G_BEGIN_DECLS
+
+OwrMediaSource *_owr_uri_source_new(OwrMediaType media_type,
+    guint stream_id, OwrCodecType codec_type, gpointer uridecodebin);
+
+G_END_DECLS
+
+#endif /* __GTK_DOC_IGNORE__ */
+
+#endif /* __OWR_URI_SOURCE_PRIVATE_H__ */
