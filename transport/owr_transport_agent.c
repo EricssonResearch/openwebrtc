5f8e43b7703e1addfbcd27f6f258ccff3e3c51f8
Alessandro Decina <alessandro.d@gmail.com>
2016-02-01 14:58:25 +1100

    transport agent: optional OpenGL support
    Add gldownload before the encoder to allow both OpenGL and non-OpenGL sources

--- transport/owr_transport_agent.c
+++ transport/owr_transport_agent.c
@@ -2260,7 +2260,11 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
     g_warn_if_fail(sync_ok);
 
     if (media_type == OWR_MEDIA_TYPE_VIDEO) {
-        GstElement *flip, *queue = NULL, *encoder_capsfilter;
+        GstElement *gldownload, *flip, *queue = NULL, *encoder_capsfilter;
+
+        name = g_strdup_printf("send-input-video-gldownload-%u", stream_id);
+        gldownload = gst_element_factory_make("gldownload", name);
+        g_free(name);
 
         name = g_strdup_printf("send-input-video-flip-%u", stream_id);
         flip = gst_element_factory_make("videoflip", name);
@@ -2293,12 +2297,12 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
         g_object_set(encoder_capsfilter, "caps", caps, NULL);
         gst_caps_unref(caps);
 
-        gst_bin_add_many(GST_BIN(send_input_bin), flip, queue, encoder, encoder_capsfilter, payloader, NULL);
+        gst_bin_add_many(GST_BIN(send_input_bin), gldownload, flip, queue, encoder, encoder_capsfilter, payloader, NULL);
         if (parser) {
             gst_bin_add(GST_BIN(send_input_bin), parser);
-            link_ok &= gst_element_link_many(flip, queue, encoder, parser, encoder_capsfilter, payloader, NULL);
+            link_ok &= gst_element_link_many(gldownload, flip, queue, encoder, parser, encoder_capsfilter, payloader, NULL);
         } else
-            link_ok &= gst_element_link_many(flip, queue, encoder, encoder_capsfilter, payloader, NULL);
+            link_ok &= gst_element_link_many(gldownload, flip, queue, encoder, encoder_capsfilter, payloader, NULL);
 
         link_ok &= gst_element_link_many(payloader, rtp_capsfilter, NULL);
 
@@ -2312,9 +2316,10 @@ static void handle_new_send_payload(OwrTransportAgent *transport_agent, OwrMedia
         sync_ok &= gst_element_sync_state_with_parent(encoder);
         sync_ok &= gst_element_sync_state_with_parent(queue);
         sync_ok &= gst_element_sync_state_with_parent(flip);
+        sync_ok &= gst_element_sync_state_with_parent(gldownload);
 
         name = g_strdup_printf("video_sink_%u_%u", OWR_CODEC_TYPE_NONE, stream_id);
-        sink_pad = gst_element_get_static_pad(flip, "sink");
+        sink_pad = gst_element_get_static_pad(gldownload, "sink");
         add_pads_to_bin_and_transport_bin(sink_pad, send_input_bin,
             transport_agent->priv->transport_bin, name);
         gst_object_unref(sink_pad);
