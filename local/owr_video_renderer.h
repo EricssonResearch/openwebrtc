410c7d9ffe61a160faf8dc7f3937c14e6c8c52cc
Per-Erik Brodin <per-erik.brodin@ericsson.com>
2017-02-12 13:17:43 -0800

    local: Fix OwrVideoRendererRequestContextCallback introspection

--- local/owr_video_renderer.h
+++ local/owr_video_renderer.h
@@ -62,8 +62,8 @@ GType owr_video_renderer_get_type(void) G_GNUC_CONST;
 
 OwrVideoRenderer *owr_video_renderer_new(const gchar *tag);
 
-typedef struct _GstContext GstContext;
-typedef GstContext* (* OwrVideoRendererRequestContextCallback) (const gchar* context_type, gpointer user_data);
+typedef struct _GstContext OwrGstContext;
+typedef OwrGstContext * (* OwrVideoRendererRequestContextCallback) (const gchar *context_type, gpointer user_data);
 void owr_video_renderer_set_request_context_callback(OwrVideoRenderer *renderer, OwrVideoRendererRequestContextCallback callback, gpointer user_data, GDestroyNotify destroy_data);
 
 G_END_DECLS
