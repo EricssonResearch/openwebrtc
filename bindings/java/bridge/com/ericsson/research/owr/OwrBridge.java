e2bf9f683b1dc27412768d1d40808c317ad37115
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-15 14:11:37 +0800

    Delete bindings directory

--- bindings/java/bridge/com/ericsson/research/owr/OwrBridge.java
+++
@@ -1,9 +0,0 @@
-package com.ericsson.research.owr;
-
-public class OwrBridge {
-    static {
-        System.loadLibrary("openwebrtc_jni");
-        System.loadLibrary("openwebrtc_bridge_jni");
-    }
-    public static native void start();
-}
